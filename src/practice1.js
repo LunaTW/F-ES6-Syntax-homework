1defea710e15b81fc8aeabf8ca07730ae9102820
LunaTW <68732727+LunaTW@users.noreply.github.com>
2020-11-10 20:41:30 +0800

    [Yue Zhang] finish

--- src/practice1.js
+++ src/practice1.js
@@ -1,2 +1,10 @@
-const parseData
+const parseData = (input) =>{
+    let columns = [];
+    input.column.map((eachLine)=>{columns.push(eachLine.name)});
+
+    return input.data.map((person)=>{return Object.assign({},...(person.map((value,index)=>{
+        return {[columns[index]] : value}
+    })))})
+}
+
 export { parseData };
